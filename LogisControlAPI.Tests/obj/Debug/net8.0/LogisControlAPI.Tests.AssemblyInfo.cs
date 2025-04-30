9cdf24db21e0d0dfb8365f9c6fba39c10e609720
rubsribs <a25994@gmail.com>
2025-04-30 20:14:39 +0100

    merge

--- LogisControlAPI.Tests/obj/Debug/net8.0/LogisControlAPI.Tests.AssemblyInfo.cs
+++ LogisControlAPI.Tests/obj/Debug/net8.0/LogisControlAPI.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogisControlAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a87df08d05452d241fd66b217a9becddc701e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c57820099ae15b1a86b013cca263526def1bcd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogisControlAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogisControlAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
