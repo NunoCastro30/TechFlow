83772e2c02a50c44418fa99cf6212a6eee78bbf8
Jorge Mendes <a18466@alunos.ipca.pt>
2025-05-01 17:31:40 +0100

    merge with main

--- LogisControlAPI.Tests/obj/Debug/net8.0/LogisControlAPI.Tests.AssemblyInfo.cs
+++ LogisControlAPI.Tests/obj/Debug/net8.0/LogisControlAPI.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogisControlAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f44fe3fca8b5cdb1b5c0e3fae9371e15e0eef00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b5465a6a68af86a4cc715ac49bb9bade29db05")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogisControlAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogisControlAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
